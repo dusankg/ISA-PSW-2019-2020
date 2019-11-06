7aa7e042e4cef9fd5bd080587b83eba8808559e4
Dusan <dusan1998kg@gmail.com>
2019-11-06 20:41:01 +0100

    Initial commit
    Prvi komit. Trebalo bi da se sada pushuju svi fajlovi potrebni za projekat i rad sa njim.

---
+++ src/main/java/com/example/demo/IsaPsw20192020Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaPsw20192020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaPsw20192020Application.class, args);
+	}
+
+}
