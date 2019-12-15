ebabf002bf61ed637f548ef7696bd9c41ea76ce5
dusankg <dusan1998kg@gmail.com>
2019-12-15 20:36:41 +0100

    Dodata pocetna faza spring-security-ja

--- isa_frontend/src/components/StartPage.vue
+++ isa_frontend/src/components/StartPage.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <h1 v-if="($route.name) ==='Home' ">Welcome to 3DE clinical center</h1>
+        <h1>Clinical center app</h1>
         <div v-if="($route.name) ==='Home' ">
         <!--For now we make like this until we split this on separate pages-->
-            <router-link to="/all">Lists of all data and forms for managing them</router-link>
-            <!--<PatientApp />
+            <!--<router-link to="/all">Lists of all data and forms for managing them</router-link>
+            1<PatientApp />
             <LoginApp/>-->
             
         </div>
@@ -29,5 +29,6 @@ export default {
 </script>
 
 <style>
+@import url(https://unpkg.com/bootstrap@4.1.0/dist/css/bootstrap.min.css);
 
 </style>
